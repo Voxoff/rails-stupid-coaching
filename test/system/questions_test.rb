c07d49aad9fc8dedeef03569ef4380314542999d
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2017-11-06 13:22:53 +0000

    yeah

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -1,14 +1,18 @@
 require "application_system_test_case"
 
 class QuestionsTest < ApplicationSystemTestCase
-  test "visiting /ask renders teh form" do
+  test "visiting /ask renders the form" do
     visit ask_url
 
     assert_selector "p", text: "Ask your coach anything"
   end
-  # test "visiting the index" do
-  #   visit questions_url
-  #
-  #   assert_selector "h1", text: "Questions"
-  # end
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+    take_screenshot
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
 end
